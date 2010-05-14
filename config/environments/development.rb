abe81ae4b62a517a99d4cd807c28c3501060dfa0
John Goodsen <jgoodsen@gandalf.local>
2010-05-13 20:22:22 -0400

    removed webrat dependency

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,6 @@ config.action_mailer.default_url_options   = { :host => DOMAIN_AND_PORT }
 
 config.gem "factory_girl", :source => "http://gemcutter.org"
 config.gem 'cucumber',    :lib => false,        :version => '>=0.3.97' unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber'))
-config.gem 'webrat',      :lib => false,        :version => '>=0.5.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/webrat'))
 config.gem 'rspec',       :lib => false,        :version => '>=1.2.6' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
 config.gem 'rspec-rails', :lib => 'spec/rails', :version => '>=1.2.6' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
 config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
