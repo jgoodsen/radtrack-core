333eaefb2665977f09f57912431f7d3acb5cf499
John Goodsen <jgoodsen@gandalf.local>
2010-05-13 21:34:15 -0400

    commented smtp_tls

--- lib/smtp_tls.rb
+++ lib/smtp_tls.rb
@@ -1,71 +1,71 @@
-require "openssl"
-require "net/smtp"
-
-# http://blog.pomozov.info/posts/how-to-send-actionmailer-mails-to-gmailcom.html
-
-Net::SMTP.class_eval do
-  private
-  def do_start(helodomain, user, secret, authtype)
-    raise IOError, 'SMTP session already started' if @started
-    raise "nil user" if user.nil?
-    raise "nil secret" if secret.nil?
-    # raise "nil authtype" if authtype.nil?
-    # check_auth_args user, secret, authtype if user or secret
-    check_auth_args user, secret if user or secret
-
-    sock = timeout(@open_timeout) { TCPSocket.open(@address, @port) }
-    @socket = Net::InternetMessageIO.new(sock)
-    @socket.read_timeout = 60 #@read_timeout
-
-    check_response(critical { recv_response() })
-    do_helo(helodomain)
-
-    raise 'openssl library not installed' unless defined?(OpenSSL)
-    starttls
-    ssl = OpenSSL::SSL::SSLSocket.new(sock)
-    ssl.sync_close = true
-    ssl.connect
-    @socket = Net::InternetMessageIO.new(ssl)
-    @socket.read_timeout = 60 #@read_timeout
-    do_helo(helodomain)
-
-    authenticate user, secret, authtype if user
-    @started = true
-  ensure
-    unless @started
-      # authentication failed, cancel connection.
-      @socket.close if not @started and @socket and not @socket.closed?
-      @socket = nil
-    end
-  end
-
-  def do_helo(helodomain)
-     begin
-      if @esmtp
-        ehlo helodomain
-      else
-        helo helodomain
-      end
-    rescue Net::ProtocolError
-      puts "************FOO"
-      if @esmtp
-        @esmtp = false
-        @error_occured = false
-        retry
-      end
-      raise
-    end
-  end
-
-  def starttls
-    getok('STARTTLS')
-  end
-
-  def quit
-    begin
-      getok('QUIT')
-    rescue EOFError
-    rescue OpenSSL::SSL::SSLError
-    end
-  end
-end
+# require "openssl"
+# require "net/smtp"
+# 
+# # http://blog.pomozov.info/posts/how-to-send-actionmailer-mails-to-gmailcom.html
+# 
+# Net::SMTP.class_eval do
+#   private
+#   def do_start(helodomain, user, secret, authtype)
+#     raise IOError, 'SMTP session already started' if @started
+#     raise "nil user" if user.nil?
+#     raise "nil secret" if secret.nil?
+#     # raise "nil authtype" if authtype.nil?
+#     # check_auth_args user, secret, authtype if user or secret
+#     check_auth_args user, secret if user or secret
+# 
+#     sock = timeout(@open_timeout) { TCPSocket.open(@address, @port) }
+#     @socket = Net::InternetMessageIO.new(sock)
+#     @socket.read_timeout = 60 #@read_timeout
+# 
+#     check_response(critical { recv_response() })
+#     do_helo(helodomain)
+# 
+#     raise 'openssl library not installed' unless defined?(OpenSSL)
+#     starttls
+#     ssl = OpenSSL::SSL::SSLSocket.new(sock)
+#     ssl.sync_close = true
+#     ssl.connect
+#     @socket = Net::InternetMessageIO.new(ssl)
+#     @socket.read_timeout = 60 #@read_timeout
+#     do_helo(helodomain)
+# 
+#     authenticate user, secret, authtype if user
+#     @started = true
+#   ensure
+#     unless @started
+#       # authentication failed, cancel connection.
+#       @socket.close if not @started and @socket and not @socket.closed?
+#       @socket = nil
+#     end
+#   end
+# 
+#   def do_helo(helodomain)
+#      begin
+#       if @esmtp
+#         ehlo helodomain
+#       else
+#         helo helodomain
+#       end
+#     rescue Net::ProtocolError
+#       puts "************FOO"
+#       if @esmtp
+#         @esmtp = false
+#         @error_occured = false
+#         retry
+#       end
+#       raise
+#     end
+#   end
+# 
+#   def starttls
+#     getok('STARTTLS')
+#   end
+# 
+#   def quit
+#     begin
+#       getok('QUIT')
+#     rescue EOFError
+#     rescue OpenSSL::SSL::SSLError
+#     end
+#   end
+# end
