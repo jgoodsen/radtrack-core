6b3ff22d0551389cb6aad10498faa2c239d7bab3
John Goodsen <jgoodsen@radsoft.com>
2010-05-15 15:24:59 -0400

    using latest installed rails version as a default for now

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,6 @@
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-if `hostname` == "radtrack"
-  RAILS_GEM_VERSION = '2.1.0' if ENV['RAILS_ENV'] == 'production'
-else
-  RAILS_GEM_VERSION = '2.3.5' unless defined?(RAILS_GEM_VERSION) || ENV['RAILS_ENV'] == 'production'
-end
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
