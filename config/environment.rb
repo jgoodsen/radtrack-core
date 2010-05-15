92e86d84a76ce520485b4c68bf2939f65b5f2638
John Goodsen <jgoodsen@radsoft.com>
2010-05-15 15:01:09 -0400

    still messing with GEM version selection

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,11 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.0' if ENV['RAILS_ENV'] == 'production'
-RAILS_GEM_VERSION = '2.3.5' unless defined?(RAILS_GEM_VERSION) || ENV['RAILS_ENV'] == 'production'
+if `hostname` == "radtrack"
+  RAILS_GEM_VERSION = '2.1.0' if ENV['RAILS_ENV'] == 'production'
+else
+  RAILS_GEM_VERSION = '2.3.5' unless defined?(RAILS_GEM_VERSION) || ENV['RAILS_ENV'] == 'production'
+end
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
