7e37d4d00e71ddd6ad6213af30f62dea31d86e03
John Goodsen <jgoodsen@radsoft.com>
2010-05-15 14:21:25 -0400

    selecting proper rails version logic

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
+RAILS_GEM_VERSION = '2.3.5' unless ENV['RAILS_ENV'] == 'production'
 RAILS_GEM_VERSION = '2.1.0' if ENV['RAILS_ENV'] == 'production'
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
