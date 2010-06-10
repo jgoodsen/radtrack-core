6c80a7df72d0ff23444917a10746a4591b91c07d
John Goodsen <jgoodsen@radsoft.com>
2010-06-09 22:06:06 -0400

    tightening up login nav and project selector dropdown

--- public/javascripts/project.js
+++ public/javascripts/project.js
@@ -15,16 +15,15 @@ $(function() {
 		function delete_project(parent) {
 		  if (confirm('Are you sure you want to remove this project?')) {
 			var project_id = $(this).attr('project_id')
-				$.post(project_url(project_id), {"_method":"delete", "authenticity_token":window._auth_token}, delete_project_callback_success.curry(parent), "json");        
+				$.post(project_url(project_id), {"_method":"delete", "authenticity_token":window._auth_token}, delete_project_callback_success, "json");        
 		  }
 		  return true;
 		}
 
-		function delete_project_callback_success(parent, data, textStatus) {
-			$(parent).remove();
+		function delete_project_callback_success(data, textStatus) {
 			return true;
 		}
-		$('img.project_delete', this).click(delete_project.curry(this));
+		$('img.project_delete', this).click(delete_project);
 	
 	};
 	
@@ -41,7 +40,8 @@ $(function() {
         $.post(
           admin_remove_user_from_project_url(project_id, user_id), 
           { "authenticity_token":window._auth_token }, 
-          function(date) {$('#user_' + user_id).remove();}, "json"
+          function(date) {$('#user_' + user_id).remove()}, 
+					"json"
         );        
 		  }
 		  return true;
