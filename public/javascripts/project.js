c861e98c1ce9c47cb2f7a3b11a59e7832834052c
@jgoodsen <jgoodsen@radsoft.com>
2010-07-17 12:54:17 -0400

    fixed unclosed div on team members list and visible dialog html for new project

--- public/javascripts/project.js
+++ public/javascripts/project.js
@@ -1,96 +1,94 @@
 function Project(project_id) {
 
-  var self = this;
+	var self = this;
 
-  this.users = [];
-  this.project_id = project_id;
+	this.users = [];
+	this.project_id = project_id;
 
-  Project.prototype.init = function() {
-    this.ajaxGetUsersForProject();
-  };
+		Project.prototype.init = function() {
+      this.ajaxGetUsersForProject();
+    };
 
-  Project.prototype.ajaxGetUsersForProjectCB = function(data, textStatus) {
-    self.users = data;
-    $('body').trigger("CurrentProject.UsersLoaded")
-  }
+	Project.prototype.ajaxGetUsersForProjectCB = function(data, textStatus) {
+		self.users = data;
+		$('body').trigger("CurrentProject.UsersLoaded")
+	}
 
-  Project.prototype.ajaxGetUsersForProject = function() {
-    $.get("/projects/" + project_id + "/users", {},
-        this.ajaxGetUsersForProjectCB, "json")
-  }
+	Project.prototype.ajaxGetUsersForProject = function() {
+		$.get("/projects/" + project_id + "/users", {},
+		this.ajaxGetUsersForProjectCB, "json")
+	}
 
   Project.prototype.getUser = function(id) {
-    for (var i = 0; i < this.users.length; i++) {
-      var user = this.users[i]
-      if (user.id == id) {
-        return user;
-      }
-    }
-    throw "No user found for id '" + id + "'"
-  }
+		for (var i=0; i < this.users.length; i++) {
+			var user = this.users[i]
+			if (user.id == id) {
+				return user;
+			}
+		}
+		throw "No user found for id '" + id + "'"
+	}
 
   Project.prototype.setUsers = function(users_json) {
-    self.users = users_json;
-  }
+		self.users = users_json;
+	}
 
 }
 
 $(function() {
 
-  $.fn.project_tout = function(options) {
+	$.fn.project_tout = function(options) {
 
-    var defaults = {};
-    var options = $.extend(defaults, options);
+		var defaults = {};
+		var options = $.extend(defaults, options);
 
-    var self = this;
+		var self = this;
 
-    function delete_project(parent) {
-      if (confirm('Are you sure you want to remove this project?')) {
-        var project_id = $(this).attr('project_id')
-        $.post(project_url(project_id), {
-          "_method": "delete",
-          "authenticity_token": window._auth_token
-        },
-            delete_project_callback_success, "json");
-      }
-      return true;
-    }
+		function delete_project(parent) {
+			if (confirm('Are you sure you want to remove this project?')) {
+				var project_id = $(this).attr('project_id')
+				$.post(project_url(project_id), {
+					"_method": "delete",
+					"authenticity_token": window._auth_token
+				},
+				delete_project_callback_success, "json");
+			}
+			return true;
+		}
 
-    function delete_project_callback_success(data, textStatus) {
-      return true;
-    }
+		function delete_project_callback_success(data, textStatus) {
+			return true;
+		}
+		$('img.project_delete', this).click(delete_project);
 
-    $('img.project_delete', this).click(delete_project);
+	};
 
-  };
+	$.fn.project_user = function(options) {
 
-  $.fn.project_user = function(options) {
+		var defaults = {};
+		var options = $.extend(defaults, options);
 
-    var defaults = {};
-    var options = $.extend(defaults, options);
-
-    function remove_user_from_project() {
-      if (confirm('Are you sure you want to remove this user from the project?')) {
-        var user_id = $(this).attr('id').replace(/[^\d]+/g, '');
-        $.post(
-            admin_remove_user_from_project_url(project_id, user_id), {
-          "authenticity_token": window._auth_token
-        },
-            function(date) {
-              $('#user_' + user_id).remove()
-            },
-            "json");
-      }
-      return true;
-    }
-
-    $('img.project_user_delete', this).click(remove_user_from_project);
+		function remove_user_from_project() {
+			if (confirm('Are you sure you want to remove this user from the project?')) {
+				var user_id = $(this).attr('id').replace(/[^\d]+/g, '');
+				$.post(
+				admin_remove_user_from_project_url(project_id, user_id), {
+					"authenticity_token": window._auth_token
+				},
+				function(date) {
+					$('#user_' + user_id).remove()
+				},
+				"json");
+			}
+			return true;
+		}
+		$('img.project_user_delete', this).click(remove_user_from_project);
 
 
-    return $(this);
-  }
+		return $(this);
+	}
 
-  $('#myprojects .project').project_tout();
-  $('div.project_user').project_user();
+	$('#myprojects .project').project_tout();
+	$('div.project_user').project_user();
 
 });
